6cbd803d8f34ccb08253e49c072f1b5c58efcbaf
nakayama-bird <hi.hi.hikaru0603@gmail.com>
2025-12-23 18:35:10 +0900

    add problem3

--- problem3/problem3.rb
+++ problem3/problem3.rb
@@ -1,15 +1,116 @@
 n = 600_851_475_143
-squrt_num = 2..Integer.sqrt(n)
-prime_num = []
+prime_factors = []
 
-squrt_num.each do |i|
-  next unless n % i != 0
+if n.even?
+  prime_factors << 2
+  loop do
+    break unless n.even?
 
-  prime_num << i
-  n /= i
+    n /= 2
+  end
 end
-pp prime_num_1
 
-prime_num_1.each do |i|
-  primeMath.log2(i) if i.even?
+if (n % 3).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
 end
+
+if (n % 5).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 7).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 11).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 13).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 17).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 19).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 23).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 29).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 31).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+if (n % 37).zero?
+  prime_factors << 3
+  loop do
+    break unless (n % 3).zero?
+
+    n /= 3
+  end
+end
+
+require 'prime'
+pp "#{n} is prime: #{n.prime?}"
+
+pp prime_factors
+pp n
